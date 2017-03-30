ef08d485ee149b6b17e2774c0c96ddf08eca1724
ejunjsh <junjie.shao@ericsson.com>
2017-03-30 16:58:27 +0800

    complete simplefactory

---
+++ README.md
@@ -0,0 +1,1 @@
+http://www.cnblogs.com/wuyuegb2312/archive/2013/04/09/3008320.html
---
+++ SimpleFactory.py
@@ -0,0 +1,54 @@
+class Operation(object):
+    def GetResult(self):
+        pass
+
+class OperationAdd(Operation):
+    def GetResult(self):
+        return self.op1+self.op2
+
+class OperationSub(Operation):
+    def GetResult(self):
+        return self.op1-self.op2
+
+class OperationMul(Operation):
+    def GetResult(self):
+        return self.op1*self.op2
+
+class OperationDiv(Operation):
+    def GetResult(self):
+        try:
+            result=self.op1/self.op2
+            return result
+        except:
+            print "error:divided by zero."
+            return 0
+
+
+class OperationUndef(Operation):
+    def GetResult(self):
+        print "Undefine operation."
+        return 0
+
+class OperationFactory:
+    operation={}
+    operation["+"]=OperationAdd();
+    operation["-"]=OperationSub();
+    operation["/"]=OperationDiv();
+    operation["*"]=OperationMul();
+    def createOperatioin(self,ch):
+        if ch in self.operation:
+            op=self.operation[ch]
+        else:
+            op=OperationUndef()
+        return op
+
+if __name__=="__main__":
+    op=raw_input("operator: ")
+    opa=input("a: ")
+    opb=input("b: ")
+    factory=OperationFactory()
+    cal=factory.createOperatioin(op)
+    cal.op1=opa
+    cal.op2=opb
+    print cal.GetResult()
+    
